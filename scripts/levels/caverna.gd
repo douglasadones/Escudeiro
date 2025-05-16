f85b7db09a85beb433590d62cfafa194bce282c0
k1luc <ka.luc@hotmail.com>
2025-05-15 21:57:43 -0300

    Cria cena da carverna e adega e menu de pausa

--- scripts/levels/caverna.gd
+++ scripts/levels/caverna.gd
@@ -6,3 +6,9 @@ class_name Caverna
 
 func _ready() -> void:
 	Global.current_scene_path = scene_path
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Escudeiro":
+		Global.current_scene_path = "res://scenes/levels/Adega/adega.tscn"
+		transition_screen.fade_in()
